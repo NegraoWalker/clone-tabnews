a273f81834844b67e8f6bef76cb97c38e7ee467d
Walker <walker.esteves.negrao@gmail.com>
2026-05-10 10:53:14 -0300

    Dia das mães

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
 function Home() {
-    return <h1>Teste</h1>
+  return (
+    <div style={{ textAlign: "center", fontFamily: "Georgia, serif", padding: "40px" }}>
+      <img
+        src="/mãe.png"
+        alt="Foto da mãe"
+        style={{ width: "300px", borderRadius: "12px" }}
+      />
+      <h1 style={{ color: "#c0395b", fontSize: "2.5rem", marginTop: "24px" }}>
+        Feliz Dia das Mães! 💐
+      </h1>
+      <p style={{ fontSize: "1.2rem", color: "#555", maxWidth: "500px", margin: "0 auto" }}>
+        Obrigado por tudo, mãe. Te amo muito! ❤️
+      </p>
+    </div>
+  );
 }
 
 export default Home;
